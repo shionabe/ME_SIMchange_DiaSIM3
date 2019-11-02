437fad27b428d97d29314039d9a032b64816e85a
ABE <abe@MacBook-Pro.local>
2019-11-02 18:06:35 +0900

    コミットテスト

--- Laravel_sample/app/Http/Controllers/DiaController.php
+++ Laravel_sample/app/Http/Controllers/DiaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HellController extends Controller
+class DiaController extends Controller
 {
     //
     // public function index(){
