41f6686f0bda9d9abf99d8f5f97a6a71d1931db1
ABE <abe@MacBook-Pro.local>
2019-11-02 17:54:54 +0900

    MVCモデルテスト用にコミットします。

---
+++ Laravel_sample/app/Http/Controllers/DiaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HellController extends Controller
+{
+    //
+    // public function index(){
+    //   return view('hell');
+    // }
+    // public function elen(){
+    //   return view('shingeki');
+    // }
+    public function Diagnosis(){
+      return view('Dia');
+    }
+
+
+}
--- Laravel_sample/routes/web.php
+++ Laravel_sample/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('hello','HellController@index');
+// Route::get('kyozin','HellController@elen');
+Route::get('top','DiaController@Diagnosis');
